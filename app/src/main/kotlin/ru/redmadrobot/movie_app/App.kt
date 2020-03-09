20187cdb319157e267b02e23d445c95c0d27cf14
Gleb Levinkov (intern) <bigman212@yandex.ru>
2020-03-10 05:03:08 +1000

    (dev) init Timber

--- app/src/main/kotlin/ru/redmadrobot/movie_app/App.kt
+++ app/src/main/kotlin/ru/redmadrobot/movie_app/App.kt
@@ -4,6 +4,12 @@ import android.app.Application
 import ru.redmadrobot.common.di.AppProvider
 import ru.redmadrobot.common.di.DaggerApplication
 import ru.redmadrobot.movie_app.di.AppComponent
+import timber.log.Timber
+
+import timber.log.Timber.DebugTree
+
+
+
 
 class App : Application(), DaggerApplication {
     lateinit var appComponent: AppComponent
@@ -11,6 +17,10 @@ class App : Application(), DaggerApplication {
     override fun onCreate() {
         super.onCreate()
         appComponent = AppComponent.Builder.build(this, "http://example.com")
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 
     override fun getApplicationProvider(): AppProvider = appComponent
