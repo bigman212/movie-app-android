0c802d7e1cf7553aba3e77e590dcfe8d86893e60
Gleb Levinkov (intern) <bigman212@yandex.ru>
2020-03-15 01:03:49 +1000

    (di/network) подключение AndroidToolsProvider для ConnectivityManager

--- core/network/src/main/kotlin/ru/redmadrobot/core/network/di/component/NetworkComponent.kt
+++ core/network/src/main/kotlin/ru/redmadrobot/core/network/di/component/NetworkComponent.kt
@@ -1,6 +1,7 @@
 package ru.redmadrobot.core.network.di.component
 
 import dagger.Component
+import ru.redmadrobot.core.android.AndroidToolsProvider
 import ru.redmadrobot.core.network.di.NetworkProvider
 import ru.redmadrobot.core.network.di.module.MoshiModule
 import ru.redmadrobot.core.network.di.module.OkHttpClientModule
@@ -9,13 +10,15 @@ import javax.inject.Singleton
 
 @Singleton
 @Component(
+    dependencies = [AndroidToolsProvider::class],
     modules = [RetrofitModule::class, OkHttpClientModule::class, MoshiModule::class]
 )
 interface NetworkComponent : NetworkProvider {
     class Builder private constructor() {
         companion object {
-            fun build(): NetworkProvider {
+            fun build(androidToolsProvider: AndroidToolsProvider): NetworkProvider {
                 return DaggerNetworkComponent.builder()
+                    .androidToolsProvider(androidToolsProvider)
                     .build()
             }
         }
