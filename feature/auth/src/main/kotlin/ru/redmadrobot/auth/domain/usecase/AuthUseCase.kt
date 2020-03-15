f6f06ef1d382613adfc064e671447615188964fa
Gleb Levinkov (intern) <bigman212@yandex.ru>
2020-03-16 05:37:57 +1000

    (dev) сохранение и получение session_id в отдельной сущности

--- feature/auth/src/main/kotlin/ru/redmadrobot/auth/domain/usecase/AuthUseCase.kt
+++ feature/auth/src/main/kotlin/ru/redmadrobot/auth/domain/usecase/AuthUseCase.kt
@@ -1,22 +1,19 @@
 package ru.redmadrobot.auth.domain.usecase
 
 import android.annotation.SuppressLint
-import android.content.SharedPreferences
 import io.reactivex.Single
 import ru.redmadrobot.auth.data.entities.UserCredentials
 import ru.redmadrobot.auth.data.repository.AuthRepository
+import ru.redmadrobot.core.network.SessionIdRepository
 import ru.redmadrobot.core.network.entities.SessionIdResponse
 import ru.redmadrobot.core.network.entities.TvShow
 import ru.redmadrobot.core.network.entities.WithPages
 import javax.inject.Inject
 
 class AuthUseCase @Inject constructor(
-    private val sPrefs: SharedPreferences,
+    private val sessionRepo: SessionIdRepository,
     private val authRepository: AuthRepository
 ) {
-    companion object {
-        private const val PREFS_SESSION_ID_KEY = "session_id_key"
-    }
 
     fun login(login: String, password: String): Single<SessionIdResponse> {
         val credentials = UserCredentials(login, password)
@@ -34,10 +31,8 @@ class AuthUseCase @Inject constructor(
      * Сохраняем полученный session_id в SharedPrefs согласно ТЗ
      */
     @SuppressLint("NewApi")
-    private fun saveSessionId(sessionId: SessionIdResponse): SessionIdResponse {
-        sPrefs.edit()
-            .putString(PREFS_SESSION_ID_KEY, sessionId.sessionId)
-            .apply()
-        return sessionId
+    private fun saveSessionId(sessionIdResponse: SessionIdResponse): SessionIdResponse {
+        sessionRepo.saveSessionId(sessionIdResponse.sessionId)
+        return sessionIdResponse
     }
 }
