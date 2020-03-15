5b4cc5c2d09fb574cf73dae2c85b4ebf99b8deb7
Gleb Levinkov (intern) <bigman212@yandex.ru>
2020-03-16 05:27:22 +1000

    (fix/common) два статуса ошибки приходят только при 401

--- common/src/main/kotlin/ru/redmadrobot/common/base/BaseViewModel.kt
+++ common/src/main/kotlin/ru/redmadrobot/common/base/BaseViewModel.kt
@@ -33,15 +33,19 @@ open class BaseViewModel @Inject constructor(context: Context) : ViewModel() {
             is HttpException.BadRequest -> {
                 when (errorResponse.statusCode) {
                     ErrorResponse.StatusCode.INVALID_API_KEY -> getContextString(R.string.common_error_invalid_api_key)
-                    ErrorResponse.StatusCode.INVALID_CREDENTIALS -> getContextString(R.string.common_invalid_credentials)
-                    ErrorResponse.StatusCode.SESSION_DENIED -> getContextString(R.string.common_error_session_denied)
                     ErrorResponse.StatusCode.INVALID_REQUEST_TOKEN -> getContextString(R.string.common_error_auth_gone_wrong)
 
                     ErrorResponse.StatusCode.UNKNOWN_ERROR -> getContextString(R.string.common_error_unknown)
                     else -> getContextString(R.string.common_error_unknown)
                 }
             }
-            is HttpException.Unauthorized -> getContextString(R.string.common_error_session_denied)
+            is HttpException.Unauthorized -> {
+                when (errorResponse.statusCode) {
+                    ErrorResponse.StatusCode.SESSION_DENIED -> getContextString(R.string.common_error_session_denied)
+                    ErrorResponse.StatusCode.INVALID_CREDENTIALS -> getContextString(R.string.common_invalid_credentials)
+                    else -> getContextString(R.string.common_error_unauthorized)
+                }
+            }
             is HttpException.NoNetworkConnection -> getContextString(R.string.error_no_internet)
             else -> {
                 Timber.e(this)
