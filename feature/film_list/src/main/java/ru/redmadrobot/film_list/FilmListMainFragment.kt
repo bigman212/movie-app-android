70ab7dd67841775267d69e922af477fb08774f17
Gleb Levinkov (intern) <bigman212@yandex.ru>
2020-03-31 22:35:43 +1000

    (dev) ViewBinding на подмогу

--- feature/film_list/src/main/java/ru/redmadrobot/film_list/FilmListMainFragment.kt
+++ feature/film_list/src/main/java/ru/redmadrobot/film_list/FilmListMainFragment.kt
@@ -2,36 +2,27 @@ package ru.redmadrobot.film_list
 
 import android.os.Bundle
 import android.text.InputType
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.EditText
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_film_main_list.*
 import ru.redmadrobot.common.base.BaseFragment
+import ru.redmadrobot.common.extensions.viewBinding
+import ru.redmadrobot.film_list.databinding.FragmentFilmMainListBinding
 import ru.redmadrobot.film_list.di.component.FilmListComponent
-import javax.inject.Inject
 
-class FilmListMainFragment : BaseFragment() {
-    @Inject
-    internal lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    private lateinit var viewModel: FilmListViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_film_main_list, container, false) as ViewGroup
-    }
+class FilmListMainFragment : BaseFragment(R.layout.fragment_film_main_list) {
+    private val binding: FragmentFilmMainListBinding by viewBinding()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
 
         initDagger()
 //        initViewModel()
 //        initViews()
 
-        et_start_search.makeReadOnly()
-        et_start_search.setOnClickListener {
+        binding.etStartSearch.makeReadOnly()
+
+        binding.etStartSearch.setOnClickListener {
             findNavController().navigate(FilmListMainFragmentDirections.toFilmListSearchFragment())
         }
     }
